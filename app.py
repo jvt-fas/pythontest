d6a099f423e0e40cf0141cfcfe5c901f5e53b829
João Teixeira <joao.teixeira@fas-amazonia.net>
2024-04-22 23:13:08 -0400

    port

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def odata_metadata():
     return metadata, 200, {'Content-Type': 'application/xml'}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable if it's available.
+    app.run(host='0.0.0.1', port=port, debug=True)
